5831cbf87d8c4131e886ba30d66f6353d2157e83
Thuy Tran <thuytran100401@gmail.com>
2021-03-30 23:43:01 -0700

    Update gladysUserLogin.py

--- gladysUserLogin.py
+++ gladysUserLogin.py
@@ -1,5 +1,10 @@
 import io 
 
+"""
+    Student: Ngan Hoang
+    Module: gladysUserLogin
+    Description: This module logs the user in and tell if the email is valid or not.
+"""
 def login(email_address):
    while "@" not in email_address:
        email_address = input("Invalid email address. ")
